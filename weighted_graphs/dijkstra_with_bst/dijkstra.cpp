a3ced0780fffacc3e0fc33e02fa291056f9a6ae3
feranco <feranco@gmail.com>
2017-10-15 00:04:36 +0200

    updated dijkstra with set

--- weighted_graphs/dijkstra_with_bst/dijkstra.cpp
+++ weighted_graphs/dijkstra_with_bst/dijkstra.cpp
@@ -5,8 +5,17 @@
 #include <iostream>
 #include <limits>
 #include <set>
+#include <utility>
 
 using namespace std;
+
+struct Comp {
+  //for bst shortest path
+  bool operator() (const pair<int,int>& lhs, const pair<int,int>& rhs) const {
+    //important: sort by distance (the second field of pair)
+    return (lhs.second < rhs.second);
+  }
+};
 
 void shortestPath (const vector<int>& parents, vector<int>* path, int current, int start) {  
   if (parents[current] < 0) path->emplace_back(current);
@@ -20,38 +29,43 @@ int Dijkstra(const vector<vector<Node>>& graph, int source, int destination) {
   vector<int> distances(graph.size(), numeric_limits<int>::max());
   //parent of each vertex in the shortest path
   vector<int> parents(graph.size(), -1);
-  set<Node> unknown;
 
+  //open set (vertex,min_distance_to_vertex)  with vertices not explored
+  set<pair<int,int>,Comp> open_set;
+  //add s to open set and set its shortest distance to 0
   distances[source] = 0;
-  unknown.emplace(source,0);
+  open_set.emplace(make_pair(source,0));
 
-  while (!unknown.empty()) {
-    //fetch the nearest unexplored node
-    Node nearest = *(unknown.begin());
-    unknown.erase(unknown.begin());
+  while (!open_set.empty()) {
+    //fetch the nearest unexplored node u
+    const auto nearest = *(open_set.begin());
+    open_set.erase(open_set.begin());
+    int u = nearest.first;
 
     // stop when destination is reached
-    if (nearest.vertex == destination) break;
+    if (u == destination) break;
 
-    // iterate through all outcoming edges from nearest
-    for (const auto& edge : graph[nearest.vertex]){
-      if (distances[nearest.vertex] + edge.weight < distances[edge.vertex]) {
-	// update distance if possible
-	distances[edge.vertex] = distances[nearest.vertex] + edge.weight;
-	// update parent
-	parents[edge.vertex] = nearest.vertex;
+    // iterate through all outcoming edges v from u 
+    for (const auto& edge : graph[u]){
+      int v = edge.vertex, w = edge.weight;
+      if (distances[u] + w < distances[v]) {
 	// add the vertex to set or update if it was already in set
-	if (unknown.find(edge) != unknown.end()) unknown.erase(unknown.find(edge)); //log cost for the first find
-	//if (distances[edge.vertex] != numeric_limits<int>::max()) unknown.erase(unknown.find(edge)); //replace the find in O(1)
-	unknown.emplace(edge.vertex,distances[edge.vertex]);	  
+	if (distances[v] < numeric_limits<int>::max()) open_set.erase(open_set.find({v,distances[v]}));
+	// update distance if possible
+	distances[v] = distances[u] + w;
+	// update parent
+	parents[v] = u;
+	open_set.emplace(make_pair(v,distances[v]));	  
       }
     }
     
   }
+  
   vector<int> shortest_path;
   shortestPath (parents, &shortest_path, destination, source);
   for (int vertex : shortest_path) cout << vertex << " ";
   cout << "\n";
+  
   return distances[destination];
 }
 
